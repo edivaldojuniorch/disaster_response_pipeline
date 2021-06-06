d21be9635888c2b39dfb3d81b1d1466108b8e697
Edivaldo Junior <joseedivaldocg@hotmailcom>
2021-06-06 16:52:31 -0300

    feat & doc: Extract process from ETL
    the both CSV file are loaded to generate a pandas
    data frame. It join information by commun index shared
    between each one of them.
    
    A doc string is defined to the extract function, called **load_data**
    in the code.

--- data/etl_pipeline.py
+++ data/etl_pipeline.py
@@ -1,19 +1,87 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
+    """
+    Responsible for load the data inside both files referenced as arg1 and arg2.
+
+    INPUT:
+        messages_filepath: full path from CSV's messages file
+        categories_filepath: full path from CSV'sthe categories file
+
+    OUTPUT:
+        df: a data frame with the loaded files together
+
+    """
+    # load messages dataset
+    messages_file_path_and_name = messages_filepath
+    messages = pd.read_csv(messages_file_path_and_name)
+
+    # load categories dataset
+    categories_file_path_and_name = categories_filepath
+    categories = pd.read_csv(categories_file_path_and_name)
+    categories.set_index('id')
 
 
+    # merge datasets
+    df = messages.merge(categories,left_index=True, right_index=True)
+    df.drop("id_y", axis=1)
+    df.rename(columns={"id_x": "id"})
 
+    # create a dataframe of the 36 individual category columns
+    # get the firts cell to extract the columns labels
+    col_title_to_split = categories[categories["id"] == 2]["categories"]
+
+    # split the first cell value by ";" and get a list
+    col_title  = col_title_to_split.str.split(pat=";",expand=False).tolist()
+
+    # Go through the whole df cell from the `certogories` columns to generate the newers columns
+    categories[col_title[0]] = categories.categories.str.split(";",expand=True)
+
+    # select the first row of the categories dataframe
+    col_title_to_split = categories[categories["id"] == 2]["categories"]
+
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+
+    # split the first cell value by ";" and get a list
+    col_title  = col_title_to_split.str.split(pat=";",expand=False).tolist()
+
+    # Go through the whole df cell from the `certogories` columns to generate the newers columns
+    categories[col_title[0]] = categories.categories.str.split(";",expand=True)
+
+    for column in categories:
+
+        # columns to avoid 
+        if column != "id" and column != "categories":
+
+            # set each value to be the last character of the string
+            categories[column] = categories[column].str[-1]
+
+            # convert column from string to numeric
+            categories[column] = pd.to_numeric(categories[column])
     
-    pass
+
+    # drop the original categories column from `df`
+    df = df.drop(['categories', 'id_y'],axis=1)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = df.merge(categories,left_index=True, right_index=True)
+
+    return df
 
 
 def clean_data(df):
+
     pass
 
 
 def save_data(df, database_filename):
+
     pass  
 
 
@@ -24,6 +92,7 @@ def main():
 
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
+
         df = load_data(messages_filepath, categories_filepath)
 
         print('Cleaning data...')
--- data/op2.py
+++
@@ -1,5 +0,0 @@
-def hello1():
-    return print("op2")
-
-if __name__ == "__main__":
-    hello1()
